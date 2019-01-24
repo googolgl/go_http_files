ae6a84a539eb758c71f62857a84b5855e9715f0d
googol <git config --global user.email>
2019-01-24 15:00:03 +0200

    refactoring, implementation thumbnail

--- README.md
+++ README.md
@@ -1,30 +1,20 @@
 # go_http_files
 Easy http file server that allows to upload/download files.
+For images server can create thumbnail on default size 300x200. 
+
 You can send files using different ways.
 
-##First
-Use *curl* command
+### First
 
-* Request:
+Use *curl* command:
+
 ```shell
 curl -F "uploadFile=@/my/dir/image.jpeg" -F "pathFile=test" http://localhost:8081/upload
 ```
 
-* Response:
-```json
-{
-    "mime":"image/jpeg",
-    "url":"http://localhost:8081/files/test/image.jpeg",
-    "pathfile":"/files/test/",
-    "namefile":"image.jpeg",
-    "size":7285,
-    "status":"Success"
-}
+### Second
 
-```
-
-##Second
-From html form
+From html form:
 
 ```html
 <form enctype="multipart/form-data" action="http://127.0.0.1:8081/upload" method="post">
@@ -33,3 +23,24 @@ From html form
     <input type="submit" value="upload" />
 </form>
 ```
+
+### Response on request in JSON format:
+
+```json
+{
+    "url":"http://127.0.0.1:8081",
+    "mime":"image/jpeg",
+    "original":{
+        "name":"1.jpg",
+        "path":"/files/test/",
+        "size":273670
+    },
+    "thumbnail":{
+        "name":"1.jpg",
+        "path":"/files/test/thumbnail/",
+        "size":20912
+    },
+    "status":"Success"}
+}
+```
+
--- main.go
+++ main.go
@@ -3,34 +3,45 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"image"
+	"image/color"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/disintegration/imaging"
 )
 
-type respData struct {
-	Mime     string `json:"mime,omitempty"`
-	URL      string `json:"url,omitempty"`
-	PathFile string `json:"pathfile,omitempty"`
-	NameFile string `json:"namefile,omitempty"`
-	Size     int64  `json:"size,omitempty"`
-	Status   string `json:"status"`
+type RespData struct {
+	URL       string `json:"url,omitempty"`
+	Mime      string `json:"mime,omitempty"`
+	Original  File   `json:"original,omitempty"`
+	Thumbnail File   `json:"thumbnail,omitempty"`
+	Status    string `json:"status"`
+}
+
+type File struct {
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
+	Size int64  `json:"size,omitempty"`
 }
 
 var (
-	maxUploadSize = flag.Int64("maxsize", 9437184, "max allowed size uploaded files")
+	maxUploadSize = flag.Int64("maxsize", 9437184, "max allowed size uploaded files in Byte")
 	uploadPath    = flag.String("path", "/files/", "root upload path")
 	host          = flag.String("host", "127.0.0.1:8081", "server address")
 	domain        = flag.String("domain", "", "domain address (with http://)")
+	thumwidth     = flag.Int64("thumwidth", 300, "thumbnail width")
+	thumhigher    = flag.Int64("thumhigher", 200, "thumbnail higher")
 )
 
 func main() {
 	flag.Parse()
 
-	http.Handle("/files", http.StripPrefix("/files", http.FileServer(http.Dir("."+*uploadPath))))
+	http.Handle("/files/", http.StripPrefix("/files", http.FileServer(http.Dir("."+*uploadPath))))
 	http.HandleFunc("/upload", uploadFileHandler())
 	log.Print("Server started on " + *host + " ...")
 	log.Fatal(http.ListenAndServe(*host, nil))
@@ -38,13 +49,21 @@ func main() {
 
 func uploadFileHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var rdata respData
+		var rdata RespData
 
 		// validate max Reguest size
 		r.Body = http.MaxBytesReader(w, r.Body, *maxUploadSize)
-		err := r.ParseMultipartForm(*maxUploadSize)
+		cSize, err := strconv.ParseInt(r.Header.Get("Content-Length"), 10, 64)
 		if err != nil {
-			response(w, 413, "File too big", rdata)
+			response(w, http.StatusCreated, "Invalid request", rdata)
+			return
+		}
+		if cSize > *maxUploadSize {
+			response(w, http.StatusCreated, "File too big", rdata)
+		}
+		//rdata.Original.Size = cSize
+		err = r.ParseMultipartForm(*maxUploadSize)
+		if err != nil {
 			return
 		}
 
@@ -52,13 +71,7 @@ func uploadFileHandler() http.HandlerFunc {
 			response(w, http.StatusCreated, "Error post data", rdata)
 			return
 		}
-		rdata.PathFile = *uploadPath + r.FormValue("pathFile") + "/"
-
-		rdata.Size, err = strconv.ParseInt(r.Header.Get("Content-Length"), 10, 64)
-		if err != nil {
-			response(w, http.StatusCreated, "Strange client", rdata)
-			return
-		}
+		rdata.Original.Path = *uploadPath + r.FormValue("pathFile") + "/"
 
 		file, handle, err := r.FormFile("uploadFile")
 		if err != nil {
@@ -66,23 +79,38 @@ func uploadFileHandler() http.HandlerFunc {
 			return
 		}
 		defer file.Close()
-		rdata.NameFile = handle.Filename
-
+		rdata.Original.Name = handle.Filename
 		rdata.Mime = handle.Header.Get("Content-Type")
 		switch rdata.Mime {
-		case "image/jpeg", "image/jpg", "image/gif", "image/png":
-			saveFile(w, file, rdata)
-		case "application/pdf", "application/msword":
-			saveFile(w, file, rdata)
+		case "image/jpeg", "image/jpg", "image/gif", "image/png", "image/bmp", "image/tif":
+			saveFile(file, rdata.Original.Path, rdata.Original.Name)
+			rdata.Thumbnail.Name = handle.Filename
+			rdata.Thumbnail.Path = rdata.Original.Path + "thumbnail/"
+			saveThumbnail(rdata)
+		case "application/pdf", "application/msword", "application/octet-stream":
+			saveFile(file, rdata.Original.Path, rdata.Original.Name)
+			rdata.Thumbnail.Name = "application.svg"
+			rdata.Thumbnail.Path = *uploadPath
 		default:
 			log.Println("Err! Invalid file type:", rdata.Mime)
 			response(w, http.StatusCreated, "Invalid file type", rdata)
 			return
 		}
+
+		rdata.Original.Size = fileSize(rdata.Original.Path + rdata.Original.Name)
+		rdata.Thumbnail.Size = fileSize(rdata.Thumbnail.Path + rdata.Thumbnail.Name)
+
+		if len(*domain) > 0 {
+			rdata.URL = *domain
+		} else {
+			rdata.URL = "http://" + *host
+		}
+		response(w, http.StatusCreated, "Success", rdata)
 	})
 }
 
-func saveFile(w http.ResponseWriter, file multipart.File, rdata respData) {
+//func saveFile(file multipart.File, rdata respData) {
+func saveFile(file multipart.File, pathFile string, fname string) {
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Println("Err!", err)
@@ -90,9 +118,9 @@ func saveFile(w http.ResponseWriter, file multipart.File, rdata respData) {
 	}
 
 	// create directory if not exist
-	_, err = ioutil.ReadDir("." + rdata.PathFile)
+	_, err = ioutil.ReadDir("." + pathFile)
 	if err != nil {
-		pathErr := os.MkdirAll("."+rdata.PathFile, 0777)
+		pathErr := os.MkdirAll("."+pathFile, 0777)
 		if pathErr != nil {
 			log.Println("Err!", err)
 			return
@@ -100,21 +128,36 @@ func saveFile(w http.ResponseWriter, file multipart.File, rdata respData) {
 	}
 
 	// write file
-	err = ioutil.WriteFile("."+rdata.PathFile+rdata.NameFile, data, 0666)
+	err = ioutil.WriteFile("."+pathFile+fname, data, 0666)
 	if err != nil {
 		log.Println("Err!", err)
 		return
 	}
-
-	if len(*domain) > 0 {
-		rdata.URL = *domain + rdata.PathFile + rdata.NameFile
-	} else {
-		rdata.URL = "http://" + *host + rdata.PathFile + rdata.NameFile
-	}
-	response(w, http.StatusCreated, "Success", rdata)
 }
 
-func response(w http.ResponseWriter, code int, message string, rd respData) {
+func saveThumbnail(rdata RespData) {
+	_, err := ioutil.ReadDir("." + rdata.Thumbnail.Path)
+	if err != nil {
+		pathErr := os.MkdirAll("."+rdata.Thumbnail.Path, 0777)
+		if pathErr != nil {
+			log.Println("Err!", err)
+			return
+		}
+	}
+	src, err := imaging.Open("." + rdata.Original.Path + rdata.Original.Name)
+	if err != nil {
+		log.Println("Err!", err)
+	}
+	src = imaging.Fill(src, int(*thumwidth), int(*thumhigher), imaging.Center, imaging.Lanczos)
+	dst := imaging.New(int(*thumwidth), int(*thumhigher), color.NRGBA{0, 0, 0, 0})
+	dst = imaging.Paste(dst, src, image.Pt(0, 0))
+	err = imaging.Save(dst, "."+rdata.Thumbnail.Path+rdata.Thumbnail.Name)
+	if err != nil {
+		log.Println("Err!", err)
+	}
+}
+
+func response(w http.ResponseWriter, code int, message string, rd RespData) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	rd.Status = message
 	jsonData, err := json.Marshal(rd)
@@ -124,4 +167,12 @@ func response(w http.ResponseWriter, code int, message string, rd respData) {
 	}
 	w.WriteHeader(code)
 	w.Write([]byte(jsonData))
+}
+
+func fileSize(path string) int64 {
+	fi, err := os.Stat("." + path)
+	if err != nil {
+		log.Println("Err!", err)
+	}
+	return fi.Size()
 }
